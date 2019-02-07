2052a38c67478769d99b886d81afd951235448b3
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-02-07 19:51:27 +0000

    Faffing with website

--- website/index.php
+++ website/index.php
@@ -3,26 +3,27 @@
 <title>realhats</title>
 <style type='text/css'>
 body {background-color:#050607;font-family:monospace;text-align:center}
-.head {color:#FF00CC;font-size:30mm;padding-left:100px}
+.head {color:#CC00FF;font-size:50pt;padding-left:100px}
+.head u {text-decoration:none;color:#FF00CC}
 .links {font-size:30pt;color:#00CCFF}
 a,a:active,a:visited,a:link {color:#FFFFFF}
 a:hover {color:#CCFF00}
 .us {position:fixed;bottom:5px;color:#FFCC00;font-size:30px;width:100%}
-img.realhat {width:100px;position:relative}
+img.realhat {width:60px;position:relative;margin-right:-60px}
 </style>
 </head>
 <body>
-<div class='head'>realhats<?php
+<div class='head'>\usepackage{<u>realhats</u><?php
 $hats = Array(
-    "<img class='realhat' style='left:-62px;top:-25px' src='realhats-beret.png'>",
-    "<img class='realhat' style='left:-62px;top:-25px' src='realhats-scottish.png'>",
-    "<img class='realhat' style='left:-78px;top:-50px' src='realhats-witch.png'>",
-    "<img class='realhat' style='left:-65px;top:-45px' src='realhats-santa.png'>",
-    "<img class='realhat' style='left:-80px;top:-40px' src='realhats-sombrero.png'>",
-    "<img class='realhat' style='left:-85px;top:-30px' src='realhats-tophat.png'>"
+    "<img class='realhat' style='left:-40px;top:-15px' src='realhats-beret.png'>",
+    "<img class='realhat' style='left:-40px;top:-15px' src='realhats-scottish.png'>",
+    "<img class='realhat' style='left:-52px;top:-30px' src='realhats-witch.png'>",
+    "<img class='realhat' style='left:-42px;top:-25px' src='realhats-santa.png'>",
+    "<img class='realhat' style='left:-51px;top:-20px' src='realhats-sombrero.png'>",
+    "<img class='realhat' style='left:-55px;top:-12px' src='realhats-tophat.png'>"
 );
 echo($hats[array_rand($hats)]);
-?></div>
+?>}</div>
 <div class='links'>
 <a href='https://github.com/mscroggs/realhats'>GitHub</a>
 ~
