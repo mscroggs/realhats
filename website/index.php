4d57fd2ac0360e867e278b252dad37015fdc2bcd
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-02-08 09:13:30 +0000

    Stupid LaTeX markup

--- website/index.php
+++ website/index.php
@@ -5,10 +5,10 @@
 body {background-color:#050607;font-family:monospace;text-align:center}
 .head {color:#CC00FF;font-size:50pt}
 .head u {text-decoration:none;color:#FF00CC}
-.links {font-size:30pt;color:#00CCFF}
+.links {font-size:50px;color:#00CCFF}
 a,a:active,a:visited,a:link {color:#FFFFFF}
 a:hover {color:#CCFF00}
-.us {position:fixed;bottom:5px;color:#FFCC00;font-size:30px;width:100%}
+.us {position:fixed;bottom:5px;color:#FFCC00;font-size:5.0mm;width:100%}
 img.realhat {width:60px;position:relative;margin-right:-60px}
 </style>
 </head>
@@ -30,10 +30,9 @@ echo($hats[array_rand($hats)]);
 <a href='https://ctan.org/pkg/realhats'>CTAN</a>
 </div>
 <div class='us'>
-Craeted by
-<a href='https://twitter.com/mscroggs'>Matthew Scroggs</a>
-&
-<a href='https://twitter.com/pecnut'>Adam Townsend</a>
+\author{Matthew Scroggs (\url{<a href='http://www.mscroggs.co.uk'>mscroggs.co.uk</a>}, \twitter{<a href='https://twitter.com/mscroggs'>@mscroggs</a>})}
+<br />\and<br />
+\author{Adam Townsend (\url{<a href='http://www.adamtownsend.com'>adamtownsend.com</a>}, \twitter{<a href='https://twitter.com/pecnut'>@pecnut</a>})}
 </div>
 </body>
 </html>
