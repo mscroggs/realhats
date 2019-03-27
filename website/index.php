a31fef5f9a755a77ffb4f77dbaf48d8dcbd02729
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-03-27 11:57:25 +0000

    Added links to Aperiodical and mscroggs.co.uk to website

--- website/index.php
+++ website/index.php
@@ -2,13 +2,15 @@
 <head>
 <title>realhats</title>
 <style type='text/css'>
-body {background-color:#050607;font-family:monospace;text-align:center}
+body {background-color:#050607;font-family:monospace;text-align:center;margin-bottom:150px}
 .head {color:#CC00FF;font-size:50pt}
 .head u {text-decoration:none;color:#FF00CC}
 .links {font-size:50px;color:#00CCFF}
+.links-smaller {margin-top:45px;font-size:20pt;color:#00CCFF}
+.linkimg {height:42px;vertical-align:middle;margin-right:4pt}
 a,a:active,a:visited,a:link {color:#FFFFFF}
 a:hover {color:#CCFF00}
-.us {position:fixed;bottom:5px;color:#FFCC00;font-size:5.0mm;width:100%}
+.us {position:fixed;bottom:5px;color:#FFCC00;font-size:5.0mm;width:100%;}
 img.realhat {width:60px;position:relative;margin-right:-60px}
 </style>
 </head>
@@ -31,6 +33,12 @@ echo($hats[array_rand($hats)]);
 <a href='https://github.com/mscroggs/realhats'>GitHub</a>
 ~
 <a href='https://ctan.org/pkg/realhats'>CTAN</a>
+</div>
+<div class='links-smaller'>Read about realhats
+<br />
+<a href='https://aperiodical.com/2019/03/realhats-writing-a-latex-package/'><img src='aperiodical.png' class='linkimg'>The Aperiodical</a>
+<br />
+<a href='https://mscroggs.co.uk/blog/63'><img src='mscroggs.png' class='linkimg'>mscroggs.co.uk</a>
 </div>
 <div class='us'>
 \author{Matthew Scroggs (\url{<a href='http://www.mscroggs.co.uk'>mscroggs.co.uk</a>}, \twitter{<a href='https://twitter.com/mscroggs'>@mscroggs</a>})}
