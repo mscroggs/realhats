85640faf8bbe822d841dc67134b62c556c54911f
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-11-19 08:55:29 +0000

    We don't use these images on the website any more

--- website/index.php
+++ website/index.php
@@ -4,21 +4,7 @@
 <link rel=stylesheet type='text/css' href='/sty.css'>
 </head>
 <body>
-<div class='head'>\usepackage{<u>realhats</u><?php
-$hats = Array(
-    "<img class='realhat' style='left:-40px;top:-15px' src='realhats-beret.png'>",
-    "<img class='realhat' style='left:-50px;top:-25px' src='realhats-cowboy.png'>",
-    "<img class='realhat' style='left:-40px;top:-15px' src='realhats-dunce.png'>",
-    "<img class='realhat' style='left:-48px;top:-25px' src='realhats-fez.png'>",
-    "<img class='realhat' style='left:-44px;top:-15px' src='realhats-makelatexgreatagain.png'>",
-    "<img class='realhat' style='left:-42px;top:-25px' src='realhats-santa.png'>",
-    "<img class='realhat' style='left:-51px;top:-20px' src='realhats-sombrero.png'>",
-    "<img class='realhat' style='left:-55px;top:-12px' src='realhats-tophat.png'>",
-    "<img class='realhat' style='left:-52px;top:-30px' src='realhats-witch.png'>",
-    "<img class='realhat' style='left:-52px;top:-30px' src='realhats-crown.png'>"
-);
-//echo($hats[array_rand($hats)]);
-?>}</div>
+<div class='head'>\usepackage{<u>realhats</u>}</div>
 <div class='links'>
 <a href='https://github.com/mscroggs/realhats'>GitHub</a>
 ~
