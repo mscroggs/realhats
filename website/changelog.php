ab4c9c7481b845617b05e6fb4dce09d9f088f17b
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-13 20:48:54 +0100

    Add mortarboard

--- website/changelog.php
+++ website/changelog.php
@@ -1,6 +1,7 @@
 <?php
 
 $changelog = Array(
+    Array("2022-06-13","6.0",Array("Added Scottish hat", "Added police hat", "Added birthday hat", "Added mortarboard")),
     Array("2019-11-19","5.0",Array("Added Ash's hat")),
     Array("2019-11-02","4.0",Array("Vectorised the hats","Added dunce cap")),
     Array("2019-04-14","3.0",Array("Added optional parameters for `\hat` and `\usepackage` to control which hats are used.",
